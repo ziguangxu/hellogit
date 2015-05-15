3be1123ada9a3a40c1d9dcbc652c1cb999c50373
Eric <eric.xu@opentrade.net>
2015-05-15 15:00:14 -0700

    Add sum method

--- HelloWorld.scala
+++ HelloWorld.scala
@@ -4,4 +4,8 @@ Object HelloWorld {
 		def factorialHelper(x: Int, acc: Int): Int = if (x==0) acc else factorialHelper(x-1, x*acc)	
 		factorialHelper(n, 1)			
 	}
+	def sum(n: Int): Int = {
+		def sumHelper(x: Int, acc: Int): Int = if (x==0) acc else sumHelper(x-1, x+acc)	
+		sumHelper(n, 0)			
+	}
 }
